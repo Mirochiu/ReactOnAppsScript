7a030f394bfb25bd0bb3e83b958ba8287935a0ca
Yung-Hsiang Chiu <mirror.tw@gmail.com>
2022-11-07 10:27:25 +0800

    fix: remove the sensitive settings from server code

--- src/server/settings.js
+++ src/server/settings.js
@@ -1,7 +1,6 @@
 export const WEB_TITLE = '用AppsScript建立React網站';
-export const FOLDER_ID = '1YULbGK5lT3F4kmqpGRnWdKF_cFRK0kOu'; // https://drive.google.com/drive/folders/1YULbGK5lT3F4kmqpGRnWdKF_cFRK0kOu?usp=sharing
-export const SHEET_URL =
-  'https://docs.google.com/spreadsheets/d/1w6XD5OKvs4WhyV9aayC4M7E2YVfAn7ltIkrGlpws6aQ/edit#gid=0';
+export const FOLDER_ID = process.env.GOOGLE_DRIVE_FOLDER_ID;
+export const SHEET_URL = process.env.GOOGLE_SHEET_URL;
 export const RE_ACCOUNT =
   /^\w+((-\w+)|(\.\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
 export const RE_PASSWORD = /^[A-Za-z][A-Za-z0-9]{7,15}$/;
